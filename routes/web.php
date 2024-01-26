aeac9ea69ba25c3fdcd433906781545eece0ef37
MirkoCherchi <mirkocherchi1992@gmail.com>
2024-01-26 17:05:38 +0100

    feat: scaffolding

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $data = [
+        'fumetti' => config('comicsdb')
+    ];
+
+
+    return view('welcome', $data);
 });
