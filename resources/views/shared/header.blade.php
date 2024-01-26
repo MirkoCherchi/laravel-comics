ed2d2d74e76068d23f8c20596c24a278b3ab60e2
MirkoCherchi <mirkocherchi1992@gmail.com>
2024-01-26 18:13:28 +0100

    feat: header debug

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,1 +1,45 @@
-<header>Header</header>
+<header>
+    <div class="header-top">
+        <div class="header-mini container">
+            <ul class="d-flex justify-content-end gap-5 align-items-center m-0">
+                <li>dc powersm visa r</li>
+                <li><select>
+                        <option selected>Additional dc sites</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select></li>
+            </ul>
+
+        </div>
+    </div>
+    <div class="header-bottom">
+        <div class="header-big d-flex gap-5 align-items-center container">
+            <div class="h-bottom-left"><img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt=""></div>
+            <div class="h-bottom-list">
+                <ul class="d-flex gap-5 m-0">
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Comics</a></li>
+                    <li><a href="">Movies</a></li>
+                    <li><a href="">Tv</a></li>
+                    <li><a href="">Games</a></li>
+                    <li><a href="">Collectibles</a></li>
+                    <li><a href="">Videos</a></li>
+                    <li><a href="">Fans</a></li>
+                    <li><a href="">News</a></li>
+                    <li><select>
+                            <option selected class="selected">Shop</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select></li>
+                    <li>
+                        <input type="text" placeholder="search">
+                    </li>
+                </ul>
+            </div>
+
+
+        </div>
+    </div>
+</header>
